c105664bb6d11024d7d7a59a5c5f16cccf6e1007
andreyporto <andreyporto004@gmail.com>
2024-04-04 20:46:25 -0300

    Inicio banco

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+$usuario = 'root';
+$senha = '';
+$dataBase = 'login';
+$host = 'localhost';
+
+
+$mysqli = new mysqli($usuario, $senha, $dataBase, $host);
+
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <h1>Acesse a sua conta</h1>
+    <form action="" method="POST">
+
+    <p>
+    <label> Email </label>
+    <input type="text" id="email">
+    </p>
+
+    <p>
+    <label> Senha </label>
+    <input type="text" id="senha">
+    </p>
+
+    <p>
+        <button type="submit"> Entrar </button>
+    </p>
+    </form>
+
+</body>
+
+</html>                                                                     
