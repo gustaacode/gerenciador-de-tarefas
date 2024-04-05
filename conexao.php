7b26442ad2253b8187cb4a59be54f443921bebd4
andreyporto <andreyporto004@gmail.com>
2024-04-04 23:14:01 -0300

    Atualização no código

--- conexao.php
+++ conexao.php
@@ -1,10 +1,14 @@
 <?php
 $usuario = 'root';
 $senha = '';
-$dataBase = 'login';
-$host = 'localhost';
+$dataBase = 'gen_tarefas';
+$host = 'localhost';        
 
 
-$mysqli = new mysqli($usuario, $senha, $dataBase, $host);
+$mysqli = new mysqli($host, $usuario, $senha, $dataBase); //Conectando com o banco de dados 
+
+if ($mysqli->connect_error) {
+    die("Falha ao conectar o Banco de Dados" . $mysqli->connect_error); //Caso não tenha a conexão do banco, essa mensagem será exibida
+}
 
 ?>
