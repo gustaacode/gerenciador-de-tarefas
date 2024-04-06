2fc430df2518f9ef872f01dfa30fca8307f71f42
gustaacode <gustavosantos01.contato@gmail.com>
2024-04-06 02:07:15 -0300

    Interface de login finalizada com pequenas modificações

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="imagens/task_icon.svg">
     <title>Login - Gerenciador de Tarefas</title>
 </head>
 
