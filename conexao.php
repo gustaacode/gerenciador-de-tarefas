2664b93027628f635dc052635a2a5aeabde08aa2
gustaacode <gustavosantos01.contato@gmail.com>
2024-04-06 01:56:04 -0300

    Interface de login finalizada

--- conexao.php
+++ conexao.php
@@ -1,12 +1,10 @@
 <?php
-$usuario = 'root'; //Usuário ADM, pode editar o banco de dados
-$senha = ''; //Usado senha somente em ambientes de produção(empresarial, mercado de trabalho), próprio para a segurança do banco;
-$dataBase = 'gen_tarefas'; //Nome do banco de dados 
-$host = 'localhost'; //Endereço onde o banco está sendo executado
+$usuario = 'root';
+$senha = '';
+$dataBase = 'login';
+$host = 'localhost';
 
 
-$mysqli = new mysqli($host, $usuario, $senha, $dataBase); //Conectando com o banco de dados (é usado também para realizar operações e consultas no banco) 
+$mysqli = new mysqli($usuario, $senha, $dataBase, $host);
 
-if ($mysqli->connect_error) {
-    die("Falha ao conectar o Banco de Dados" . $mysqli->connect_error); //Caso não tenha a conexão do banco, essa mensagem será exibida
-}
+?>
--- index.php
+++ index.php
@@ -1,71 +1,27 @@
-<?php
-include('conexao.php'); //Inclui todo o código do arquivo na index.php;
-
-if (isset($_POST['email']) || isset($_POST['senha'])) //Verifica se exite uma entrada de dados - email e senha;
-
-    if (strlen($_POST['email']) == 0) { //Verifica a quantidade de caracteres preenchidas pelo usuário (email e senha);
-        echo 'Preencha seu e-mail!';
-    } else if (strlen($_POST['senha']) == 0) { //Verifica a quantidade de caracteres preenchidas pelo usuário (email e senha);
-        echo 'Preencha sua senha!';
-    } else {
-
-        $email = $mysqli->real_escape_string($_POST['email']); //Limpa a entrada de dados feita pelo usuário (função - real_escape_string);
-        $senha = $mysqli->real_escape_string($_POST['senha']); //Limpa a entrada de dados feita pelo usuário (função - real_escape_string);
-
-        $sql_cod = "SELECT * FROM usuario WHERE email = '$email' AND password = '$senha'"; //Seleciona os dados armazenados no banco, de email e senha;
-        $sqlQuery = $mysqli->query($sql_cod) or die("Falha na execução do código Sql: " . $mysqli->connect_error); // Verifica se os dados retornados existem no banco de dados
-
-        $quantidade = $sqlQuery->num_rows; //Retorna quantas linhas a consulta de $sqlQuery retornou;
-
-        if ($quantidade == 1) { //Verifica se o resultado retornou apenas 1 registro, logo correspondendo ao banco de dados;
-
-            $usuario = $sqlQuery->fetch_assoc(); //Associa os dados retornados na variável usuário, facilitando o acesso aos mesmos
-
-            if (!isset($_SESSION)) { //Verifica se existe uma sessão iniciada;
-                session_start(); //Inicia uma sessão caso não tenha;
-            }
-
-            $_SESSION['email'] = $usuario['email']; //Amazena o valores do banco relacionado ao ID, na sessão;
-            $_SESSION['nome'] = $usuario['nome']; //Amazena o valores do banco relacionado ao nome, na sessão;
-            header("Location: gen_tarefas.php"); //Redireciona o usuário para a tela principal;
-            exit;
-            //Basicamente a sessão deixa esses dados armazenados para que possa ser utilizado em outro momento, até mesmo em outra página; 
-        } else {
-            echo "Falha ao logar, Email ou Senha incorreto!";
-        }
-    }
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Login - Gerenciador de Tarefas</title>
 </head>
 
 <body>
-    <h1>Acesse a sua conta</h1>
-    <form action="" method="POST">
-
-        <p>
-            <label>Email</label>
-            <input type="text" id="email" name="email">
-        </p>
-
-        <p>
-            <label>Senha</label>
-            <input type="password" id="senha" name="senha">
-        </p>
-
-        <p>
-            <button type="submit">Entrar</button>
-        </p>
-    </form>
-
+    <section class="area-login">
+        <div class="login">
+            <div>
+                <img src="imagens/task_icon.svg">
+            </div>
+            <form method="POST">
+                <input type="text" name="email" placeholder="seu email" autofocus>
+                <input type="password" name="password" placeholder="sua senha">
+                <input type="submit" value="Entrar">
+            </form>
+            <p>Ainda não tem uma conta?<a href="#">Criar conta</a></p>
+        </div>
+    </section>
 </body>
 
 </html>
-
-
-andrey
