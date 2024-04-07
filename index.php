d7638663de7becbbccb8cc95e6de61cc139cc667
gustaacode <gustavosantos01.contato@gmail.com>
2024-04-07 13:17:33 -0300

    interface do gerenciador quase finalizada, falta resolver a extensão .php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="imagens/task_icon.svg">
+    <link rel="shortcut icon" href="imagens/task_icon.svg" type="image/x-icon">
     <title>Login - Gerenciador de Tarefas</title>
 </head>
 
@@ -16,17 +16,17 @@
                 <img src="imagens/task_icon.svg">
             </div>
             <form method="POST">
-                <input type="text" id="email" name="email" placeholder="seu email" autofocus>
-                <input type="password" id="password" name="password" placeholder="sua senha">
+                <input type="text" id="email" name="email" placeholder="seu email" autofocus required>
+                <input type="password" id="password" name="password" placeholder="sua senha" required>
                 <input type="submit" value="Entrar">
             </form>
             <p>Ainda não tem uma conta?<a href="#">Criar conta</a></p>
 
             <?php
-            include('conexao.php'); //Inclui todo o código do arquivo na index.php;
-
+            include ('conexao.php'); //Inclui todo o código do arquivo na index.php;
+            
             if (isset($_POST['email']) && isset($_POST['password'])) //Verifica se exite uma entrada de dados - email e senha;
-
+            
                 if (strlen($_POST['email']) == 0) { //Verifica a quantidade de caracteres preenchidas pelo usuário (email e senha);
                     echo 'Preencha seu e-mail!';
                 } else if (strlen($_POST['password']) == 0) { //Verifica a quantidade de caracteres preenchidas pelo usuário (email e senha);
@@ -35,16 +35,16 @@
 
                     $email = $mysqli->real_escape_string($_POST['email']); //Limpa a entrada de dados feita pelo usuário (função - real_escape_string);
                     $senha = $mysqli->real_escape_string($_POST['password']); //Limpa a entrada de dados feita pelo usuário (função - real_escape_string);
-
+            
                     $sql_cod = "SELECT * FROM usuario WHERE email = '$email' AND password = '$senha'"; //Seleciona os dados armazenados no banco, de email e senha;
                     $sqlQuery = $mysqli->query($sql_cod) or die("Falha na execução do código Sql: " . $mysqli->connect_error); // Verifica se os dados retornados existem no banco de dados;
-
+            
                     $quantidade = $sqlQuery->num_rows; //Retorna quantas linhas a consulta de $sqlQuery retornou;
-
+            
                     if ($quantidade == 1) { //Verifica se o resultado retornou apenas 1 registro, logo correspondendo ao banco de dados;
-
+            
                         $usuario = $sqlQuery->fetch_assoc(); //Associa os dados retornados na variável usuário, facilitando o acesso aos mesmos
-
+            
                         if (!isset($_SESSION)) { //Verifica se existe uma sessão iniciada;
                             session_start(); //Inicia uma sessão caso não tenha;
                         }
